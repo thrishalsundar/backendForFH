c7ea993987bee460f09afa2e9dbdc24f0bdd8e95
thrishalsundar <thir19426.cs@rmkec.ac.in>
2022-11-01 20:07:54 +0530

    20:07 11/01 fixed

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,13 +6,10 @@ const routes=express.Router();
 routes.get('/foods',adminViews.GetFoods);
 routes.get('/rests',adminViews.GetRests);
 routes.post('/newrest',adminViews.CreateRestaurant);
-routes.post('/newcuser',adminViews.NewCusUser);
 routes.post('/newruser',adminViews.NewResUser);
 routes.post('/newmuser',adminViews.NewMovUser);
 routes.delete('/remrest',adminViews.DeleteRestaurant);
-routes.delete('/remruser',adminViews.DeleteCusUser);
-routes.delete('/remruser',adminViews.DeleteResUser);
-routes.delete('/remmuser',adminViews.DeleteMovUser);
+routes.delete('/remuser',adminViews.DeleteUser);
 routes.get('/searchrest',adminViews.SearchRest);
 routes.get('/searchfood',adminViews.SearchFood);
 
